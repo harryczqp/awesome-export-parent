667b38639b2d3031a35d7bb9b37024a70ddf7297
harryczq <harryczq@hotmail.com>
2022-03-01 16:08:14 +0800

    update BeanScanner && add log

--- awesome-export-autoconfigure/src/main/java/io/ggit/awesome/export/spring/BeanScanner.java
+++ awesome-export-autoconfigure/src/main/java/io/ggit/awesome/export/spring/BeanScanner.java
@@ -1,11 +1,14 @@
 package io.ggit.awesome.export.spring;
 
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import cn.hutool.core.annotation.AnnotationUtil;
 import cn.hutool.core.map.MapUtil;
 import cn.hutool.core.text.CharSequenceUtil;
 import cn.hutool.core.util.ObjectUtil;
 import cn.hutool.core.util.ReflectUtil;
 import io.ggit.awesome.export.model.ScannedBean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -21,6 +24,14 @@ import java.util.Map;
  */
 public class BeanScanner implements ApplicationListener<ApplicationStartedEvent> {
 
+    private static final Logger log = LoggerFactory.getLogger(BeanScanner.class);
+
+    /**
+     * 配置需要扫描的包
+     */
+    @Value("${beanScanner.package:io.ggit.awesome}")
+    private String scannedPackage;
+
     /**
      * 扫描到的包
      */
@@ -30,14 +41,21 @@ public class BeanScanner implements ApplicationListener<ApplicationStartedEvent>
     public void onApplicationEvent(ApplicationStartedEvent event) {
         ConfigurableApplicationContext context = event.getApplicationContext();
         String[] beanDefinitionNames = context.getBeanDefinitionNames();
+        log.debug("BeanScanner->开始扫描Bean");
         for (String beanName : beanDefinitionNames) {
             Object bean = context.getBean(beanName);
             if (ObjectUtil.isNull(bean) || ObjectUtil.isNull(bean.getClass()) || ObjectUtil.isNull(bean.getClass().getPackage())) {
+                log.debug("未找到bean:{}的信息！", beanName);
                 continue;
             }
-
+            if (!bean.getClass().getPackage().getName().contains(scannedPackage)) {
+                continue;
+            }
             Method[] methods = ReflectUtil.getMethods(bean.getClass());
-
+            //移除null及没有标识的方法
+            methods = Arrays.stream(methods).filter(f ->
+                    ObjectUtil.isNotNull(f) && ObjectUtil.isNotNull(AnnotationUtil.getAnnotation(f, CanBeExported.class))
+            ).toArray(Method[]::new);
             for (Method method : methods) {
                 Class<?> parameterType = Arrays.stream(method.getParameterTypes()).findFirst().orElse(null);
                 ScannedBean scannedBean = new ScannedBean().setBean(bean).setParameterType(parameterType).setMethod(method);
@@ -46,7 +64,9 @@ public class BeanScanner implements ApplicationListener<ApplicationStartedEvent>
                 }
             }
         }
+        log.debug("BeanScanner->扫描到{}个Bean", scannedBeanMap.size());
     }
+
     /**
      * 构造scannedPackage的key
      *
@@ -56,7 +76,27 @@ public class BeanScanner implements ApplicationListener<ApplicationStartedEvent>
     private List<String> buildKey(ScannedBean scannedBean) {
         List<String> keys = new ArrayList<>();
         Class<?> beanClass = scannedBean.getBean().getClass();
-        // TODO: 2022/2/24  完善
+        //CanBeScanned 优先
+        String scannedValue = AnnotationUtil.getAnnotationValue(scannedBean.getMethod(), CanBeExported.class);
+        if (CharSequenceUtil.isNotEmpty(scannedValue)) {
+            keys.add(scannedValue);
+            return keys;
+        }
+        //实现方法上扫描
+        appendKey(beanClass, scannedBean.getMethod(), keys);
+        //接口上扫描
+        Class<?>[] interfaces = beanClass.getInterfaces();
+        for (Class<?> interfacesClass : interfaces) {
+            Method method = Arrays.stream(ReflectUtil.getMethods(interfacesClass)).filter(f -> f.getName().contains(scannedBean.getMethod().getName())).findFirst().orElse(null);
+            appendKey(interfacesClass, method, keys);
+        }
         return keys;
+    }
+
+    private void appendKey(Class<?> clazz, Method method, List<String> keys) {
+        if (clazz == null || method == null || keys == null) {
+            return;
+        }
+        // TODO: 2022/3/1 完善方法
     }
 }
---
+++ awesome-export-autoconfigure/src/main/java/io/ggit/awesome/export/spring/CanBeExported.java
@@ -0,0 +1,20 @@
+package io.ggit.awesome.export.spring;
+
+import java.lang.annotation.*;
+
+/**
+ * CanBeExported
+ *
+ * @author harryczq
+ */
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface CanBeExported {
+    /**
+     * 带有以下标签的可自动生成导出方法的key
+     *
+     * @return 导出方法的key
+     */
+    String value() default "";
+}
