8afd842b499baaf1ece17bb1b1c06db8ccd59f1a
harryczq <harryczq@hotmail.com>
2022-03-15 10:31:54 +0800

    format code

--- awesome-export-core/src/main/java/io/ggit/awesome/export/BasicExportImpl.java
+++ awesome-export-core/src/main/java/io/ggit/awesome/export/BasicExportImpl.java
@@ -4,6 +4,7 @@ import cn.hutool.core.util.ObjectUtil;
 import cn.hutool.json.JSONUtil;
 import io.ggit.awesome.export.model.ExportRequest;
 import io.ggit.awesome.export.model.ScannedBean;
+import io.ggit.awesome.export.utils.BasicUtil;
 import io.ggit.awesome.export.utils.ExcelGeneratorUtil;
 
 import java.io.File;
@@ -18,30 +19,17 @@ import java.util.Map;
 public class BasicExportImpl {
 
     public File exportFile(Map<String, Object> request) throws InvocationTargetException, IllegalAccessException {
-        ExportRequest exportRequest = castToBean(request, ExportRequest.class);
+        ExportRequest exportRequest = BasicUtil.castToBean(request, ExportRequest.class);
 
         ScannedBean beanMap = exportRequest
                 .getScannedBean();
-        Object parameter = castToBean(request, beanMap.getParameterType());
+        Object parameter = BasicUtil.castToBean(request, beanMap.getParameterType());
             Object result = beanMap.getMethod().invoke(beanMap.getBean(), parameter);
-        // TODO: 2022/3/10  getMethod
-//            List<?> list = castList(result, beanMap.getMethod());
-            File file = ExcelGeneratorUtil.writeExcel(new ArrayList<>(), exportRequest.getExportDetail());
+        // TODO: 2022/3/15  beanMap.getMethod() -> class??
+            List<?> list = BasicUtil.castList(result, );
+            File file = ExcelGeneratorUtil.writeExcel(list, exportRequest.getExportDetail());
             return file;
 
     }
 
-
-    public  <T> T castToBean(Object obj, Class<T> clazz) {
-        return JSONUtil.toBean(JSONUtil.parse(obj).toString(), clazz);
-    }
-    public  <T> List<T> castList(Object obj, Class<T> clazz) {
-        List<T> result = new ArrayList<>();
-        if (obj instanceof List<?>) {
-            for (Object o : (List<?>) obj) {
-                result.add(clazz.cast(o));
-            }
-        }
-        return result;
-    }
 }
--- awesome-export-core/src/main/java/io/ggit/awesome/export/utils/BasicUtil.java
+++ awesome-export-core/src/main/java/io/ggit/awesome/export/utils/BasicUtil.java
@@ -1,5 +1,14 @@
 package io.ggit.awesome.export.utils;
 
+import cn.hutool.core.lang.Assert;
+import cn.hutool.core.util.TypeUtil;
+import cn.hutool.json.JSONUtil;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * BasicUtil
  *
@@ -8,5 +17,46 @@ package io.ggit.awesome.export.utils;
 public class BasicUtil {
 
 
+    /**
+     * 优雅的转化为 list
+     *
+     * @param obj   对象
+     * @param clazz list类型
+     * @param <T>   list类型
+     * @return list
+     */
+    public static <T> List<T> castList(Object obj, Class<T> clazz) {
+        List<T> result = new ArrayList<>();
+        if (obj instanceof List<?>) {
+            for (Object o : (List<?>) obj) {
+                result.add(clazz.cast(o));
+            }
+        }
+        return result;
+    }
 
+    /**
+     * 获取方法返回的list类型
+     *
+     * @param method 方法信息
+     * @return list类型
+     */
+    public static Class<?> getMethodReturnListDefaultType(Method method) {
+        Assert.notNull(method, "方法不能为空，否则无法获取返回类型！");
+        Type returnType = TypeUtil.getReturnType(method);
+        Type typeArgument = TypeUtil.getTypeArgument(returnType, 0);
+        return (Class<?>) typeArgument;
+    }
+
+    /**
+     * 优雅的转化为Bean
+     *
+     * @param obj   对象
+     * @param clazz list类型
+     * @param <T>   list类型
+     * @return Bean
+     */
+    public static <T> T castToBean(Object obj, Class<T> clazz) {
+        return JSONUtil.toBean(JSONUtil.parse(obj).toString(), clazz);
+    }
 }
