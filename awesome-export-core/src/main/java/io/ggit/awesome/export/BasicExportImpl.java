5dd969839a97fa8e0c3ed7ad0a8e3d7ca05c6b3e
harryczq <harryczq@hotmail.com>
2022-03-29 09:56:04 +0800

    format export2Base64

--- awesome-export-core/src/main/java/io/ggit/awesome/export/BasicExportImpl.java
+++ awesome-export-core/src/main/java/io/ggit/awesome/export/BasicExportImpl.java
@@ -2,11 +2,12 @@ package io.ggit.awesome.export;
 
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.util.ObjectUtil;
-import cn.hutool.json.JSONUtil;
 import io.ggit.awesome.export.model.ExportRequest;
 import io.ggit.awesome.export.model.ScannedBean;
 import io.ggit.awesome.export.utils.BasicUtil;
 import io.ggit.awesome.export.utils.ExcelGeneratorUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.lang.reflect.InvocationTargetException;
@@ -18,8 +19,17 @@ import java.util.Map;
  */
 public class BasicExportImpl {
 
+    private static final Logger log = LoggerFactory.getLogger(BasicExportImpl.class);
 
-    public String export2Base64( Map<String, Object> request) {
+
+    /**
+     * 下载中心-导出为Base64
+     * 下一步需支持异步
+     *
+     * @param request 请求信息
+     * @return Base64
+     */
+    public String export2Base64(Map<String, Object> request) {
         File file = null;
         try {
             file = exportFile(request);
@@ -36,6 +46,12 @@ public class BasicExportImpl {
         return fileString;
     }
 
+    /**
+     * 导出成文件
+     *
+     * @param request 请求信息
+     * @return 文件
+     */
     private File exportFile(Map<String, Object> request) throws InvocationTargetException, IllegalAccessException {
         ExportRequest exportRequest = BasicUtil.castToBean(request, ExportRequest.class);
         ScannedBean beanMap = exportRequest
