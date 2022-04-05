cefd9415d4dd8bde1dcc4495fefaa1d9c5d8f9ae
harryczq <harryczq@hotmail.com>
2022-04-05 22:19:13 +0800

    build export2Base64

--- awesome-export-core/src/main/java/io/ggit/awesome/export/BasicExportImpl.java
+++ awesome-export-core/src/main/java/io/ggit/awesome/export/BasicExportImpl.java
@@ -6,8 +6,11 @@ import io.ggit.awesome.export.model.ExportRequest;
 import io.ggit.awesome.export.model.ScannedBean;
 import io.ggit.awesome.export.utils.BasicUtil;
 import io.ggit.awesome.export.utils.ExcelGeneratorUtil;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.io.File;
 import java.lang.reflect.InvocationTargetException;
@@ -21,6 +24,10 @@ public class BasicExportImpl {
 
     private static final Logger log = LoggerFactory.getLogger(BasicExportImpl.class);
 
+    /**
+     * 标准化日志
+     */
+    private static final String FORMAT_LOG_STRING = "通用导出接口-{}\r\n{}";
 
     /**
      * 下载中心-导出为Base64
@@ -29,15 +36,10 @@ public class BasicExportImpl {
      * @param request 请求信息
      * @return Base64
      */
-    public String export2Base64(Map<String, Object> request) {
-        File file = null;
-        try {
-            file = exportFile(request);
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }
+    @ApiOperation("下载中心-导出为Base64")
+    @PostMapping("export2Base64")
+    public String export2Base64(@RequestBody Map<String, Object> request) {
+        File file = exportFile(request);
         if (ObjectUtil.isNull(file)) {
             throw new IllegalArgumentException("文件导出失败！");
         }
@@ -52,12 +54,19 @@ public class BasicExportImpl {
      * @param request 请求信息
      * @return 文件
      */
-    private File exportFile(Map<String, Object> request) throws InvocationTargetException, IllegalAccessException {
+    private File exportFile(Map<String, Object> request)  {
         ExportRequest exportRequest = BasicUtil.castToBean(request, ExportRequest.class);
         ScannedBean beanMap = exportRequest
                 .getScannedBean();
         Object parameter = BasicUtil.castToBean(request, beanMap.getParameterType());
-        Object result = beanMap.getMethod().invoke(beanMap.getBean(), parameter);
+        Object result = null;
+        try {
+            result = beanMap.getMethod().invoke(beanMap.getBean(), parameter);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
         List<?> list = BasicUtil.castList(result, BasicUtil.getMethodReturnListDefaultType(beanMap.getMethod()));
         File file = ExcelGeneratorUtil.writeExcel(list, exportRequest.getExportDetail());
         return file;
