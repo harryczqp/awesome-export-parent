0ef07599502a666d0cf1b33a7bf7a3a27d2b845a
harryczq <harryczq@hotmail.com>
2022-04-28 12:09:03 +0800

    fix start bug

--- awesome-export-autoconfigure/src/main/java/io/ggit/awesome/export/spring/config/DownloadCenterExportAutoConfiguration.java
+++ awesome-export-autoconfigure/src/main/java/io/ggit/awesome/export/spring/config/DownloadCenterExportAutoConfiguration.java
@@ -2,19 +2,25 @@ package io.ggit.awesome.export.spring.config;
 
 import io.ggit.awesome.export.BasicExportImpl;
 import io.ggit.awesome.export.utils.BeanScanner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 
 /**
  * DownloadCenterExportAutoConfiguration
+ *
  * @author harryczqp
  */
 public class DownloadCenterExportAutoConfiguration {
     @Bean
+    @ConditionalOnMissingBean(BeanScanner.class)
     public BeanScanner beanScanner() {
         return new BeanScanner();
     }
 
     @Bean
+    @ConditionalOnBean(BeanScanner.class)
+    @ConditionalOnMissingBean(BasicExportImpl.class)
     public BasicExportImpl basicExport() {
         return new BasicExportImpl();
     }
